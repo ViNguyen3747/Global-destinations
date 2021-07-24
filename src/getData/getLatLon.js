9291f1ceb7d657124aa2f1ad3fcaaa1ecc91f335
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-07-24 14:39:52 -0500

    updated

--- src/getData/getLatLon.js
+++ src/getData/getLatLon.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const getLatLon = city => {
-    const geoCodeURL = "http://api.openweathermap.org/geo/1.0/direct?limit=1";
+    const geoCodeURL = "https://api.openweathermap.org/geo/1.0/direct?limit=1";
     const APIkey = "27cc0f9e54d73d8e67bfafb8beef78e4";
 
     return axios.get(geoCodeURL, { params: { q: city, appid: APIkey } })
